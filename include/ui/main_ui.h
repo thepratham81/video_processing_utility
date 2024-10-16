676541ecc70f00dc1d3959ec533c8b8e01e37233
Jagannath Hari <jagannathhari17@gmail.com>
2024-10-16 12:13:34 +0530

    Fix compilation for windows

--- include/ui/main_ui.h
+++ include/ui/main_ui.h
@@ -2,6 +2,13 @@
 #define MAIN_UI_H
 
 #include "SDL_video.h"
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
 #ifndef NK_NUKLEAR_H_
 #include <nuklear.h>
 #include <SDL.h>
