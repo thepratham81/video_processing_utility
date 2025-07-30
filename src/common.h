99a4ae31f1342e66d326a5e4db948219d49634c7
Jagannath Hari <jagannathhari17@gmail.com>
2025-07-30 13:09:32 +0530

    Added dependency

---
+++ src/common.h
@@ -0,0 +1,7 @@
+#ifndef COMMON_H
+#define COMMON_H
+#define IMGUI_DISABLE_OBSOLETE_FUNCTIONS 1
+#define IMGUI_IMPL_API extern "C" 
+#define IMGUI_IMPL_OPENGL_LOADER_GL3W
+#define IMGUI_USER_CONFIG "../cimconfig.h"
+#endif // COMMON_H
