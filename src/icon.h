0f344a88e16dfd7723f6abb84c8c6ec770920059
Jagannath Hari <jagannathhari17@gmail.com>
2025-08-05 04:34:48 +0530

    Added dependency

---
+++ src/icon.h
@@ -0,0 +1,24 @@
+#ifndef INCLUDE_ICON_H
+#define INCLUDE_ICON_H
+
+#define icon_rotate_90 "\ue909"
+#define icon_rotate_180 "\ue908"
+#define icon_rotate_270 "\ue907"
+#define icon_rotate_custom "\ue906"
+
+#define icon_mute "\ue900"
+#define icon_50_up "\ue901"
+#define icon_50_down "\ue902"
+
+#define icon_25_up "\ue903"
+#define icon_25_down "\ue904"
+
+#define icon_25_up "\ue903"
+#define icon_25_down "\ue904"
+
+#define icon_stero_to_mono "\ue905"
+
+#define icon_flipv "\ue90a"
+#define icon_fliph "\ue90b"
+
+#endif //INCLUDE_ICON_H
