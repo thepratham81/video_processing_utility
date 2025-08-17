c83b815d8858e600536f380e6a197899e2fa3de5
Jagannath Hari <jagannathhari17@gmail.com>
2025-08-17 21:35:21 +0530

    Added simple video player

--- src/main.c
+++ src/main.c
@@ -15,6 +15,10 @@
 #include <GL/gl.h>
 #include <GLFW/glfw3.h>
 
+#include <gst/gst.h>
+#include <gst/video/video.h>
+#include <gst/app/app.h>
+
 #define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
 #include "cimgui.h"
 #include "cimgui_impl.h"
@@ -928,9 +932,233 @@ void show_preview_window(int angle,bool fliph,bool flipv)
     igEndGroup();
 }
 
+typedef struct _Pipeline
+{
+    GstState pipeState;
+    char stream_uri[1024];
+    bool loop;
+    GstElement* pipeline;
+    GstElement* videosrc;
+    GstElement* convert;
+    GstElement* scale;
+    GstElement* appsink;
+    
+    // Audio elements
+    GstElement* audioconvert;
+    GstElement* audioresample;
+    GstElement* audiosink;
+    
+    GstBus* bus;
+    GstMessage* msg;
+} Pipeline;
+
+static void pad_added_handler(GstElement *src, GstPad *new_pad, Pipeline *data)
+{
+    GstPad *sink_pad = NULL;
+    GstPadLinkReturn ret;
+    GstCaps *new_pad_caps = NULL;
+    GstStructure *new_pad_struct = NULL;
+    const gchar *new_pad_type = NULL;
+    
+    g_print("Received new pad '%s' from '%s':\n", GST_PAD_NAME(new_pad), GST_ELEMENT_NAME(src));
+    
+    /* Check the new pad's type */
+    new_pad_caps = gst_pad_get_current_caps(new_pad);
+    new_pad_struct = gst_caps_get_structure(new_pad_caps, 0);
+    new_pad_type = gst_structure_get_name(new_pad_struct);
+    
+    if (g_str_has_prefix(new_pad_type, "video/x-raw") || g_str_has_prefix(new_pad_type, "video/"))
+    {
+        /* This is a video pad */
+        sink_pad = gst_element_get_static_pad(data->convert, "sink");
+        
+        /* If our video converter is already linked, we have nothing to do here */
+        if (gst_pad_is_linked(sink_pad)) {
+            g_print("  Video pad already linked. Ignoring.\n");
+            goto exit;
+        }
+        
+        /* Attempt the video link */
+        ret = gst_pad_link(new_pad, sink_pad);
+        if (GST_PAD_LINK_FAILED(ret)) {
+            g_print("  Video link failed (type '%s').\n", new_pad_type);
+        } else {
+            g_print("  Video link succeeded (type '%s').\n", new_pad_type);
+        }
+    }
+    else if (g_str_has_prefix(new_pad_type, "audio/x-raw") || g_str_has_prefix(new_pad_type, "audio/")) {
+        /* This is an audio pad */
+        sink_pad = gst_element_get_static_pad(data->audioconvert, "sink");
+        
+        /* If our audio converter is already linked, we have nothing to do here */
+        if (gst_pad_is_linked(sink_pad)) {
+            g_print("  Audio pad already linked. Ignoring.\n");
+            goto exit;
+        }
+        
+        /* Attempt the audio link */
+        ret = gst_pad_link(new_pad, sink_pad);
+        if (GST_PAD_LINK_FAILED(ret)) {
+            g_print("  Audio link failed (type '%s').\n", new_pad_type);
+        } else {
+            g_print("  Audio link succeeded (type '%s').\n", new_pad_type);
+        }
+    }
+    else {
+        g_print("  It has type '%s' which is not video or audio. Ignoring.\n", new_pad_type);
+        goto exit;
+    }
+
+exit:
+    /* Unreference the new pad's caps, if we got them */
+    if (new_pad_caps != NULL)
+        gst_caps_unref(new_pad_caps);
+    /* Unreference the sink pad */
+    if (sink_pad != NULL)
+        gst_object_unref(sink_pad);
+}
+
+bool create_pipeline(Pipeline *pipe)
+{
+    // strcpy(pipe->stream_uri,"file:///home/user/input.mkv");
+    strcpy(pipe->stream_uri,"file:///home/user/Videos/input.mp4");
+    pipe->loop = true;
+    pipe->pipeline = gst_pipeline_new("imgui_pipeline");
+    
+    // Video elements
+    pipe->videosrc = gst_element_factory_make("uridecodebin", "videosrc0");
+    pipe->convert = gst_element_factory_make("videoconvert", "videoconvert0");
+    pipe->scale = gst_element_factory_make("videoscale", "videoscale0");
+    pipe->appsink = gst_element_factory_make("appsink", "appsink0");
+    
+    // Audio elements
+    pipe->audioconvert = gst_element_factory_make("audioconvert", "audioconvert0");
+    pipe->audioresample = gst_element_factory_make("audioresample", "audioresample0");
+    pipe->audiosink = gst_element_factory_make("autoaudiosink", "audiosink0");
+    
+    if (!pipe->pipeline || !pipe->videosrc || !pipe->convert || !pipe->scale || !pipe->appsink ||
+        !pipe->audioconvert || !pipe->audioresample || !pipe->audiosink)
+    {
+        g_printerr("Not all elements could be created.\n");
+        return false;
+    }
+    g_object_set(pipe->videosrc, "uri", pipe->stream_uri, NULL);
+   g_object_set(pipe->scale, 
+    "add-borders", TRUE,    // Maintain original ratio with borders
+    "method", 1,            // Good quality scaling
+    NULL);
+    // Add all elements to the pipeline
+    gst_bin_add_many(GST_BIN(pipe->pipeline), 
+                     pipe->videosrc, pipe->convert, pipe->scale, pipe->appsink,
+                     pipe->audioconvert, pipe->audioresample, pipe->audiosink,
+                     NULL);
+    
+    g_signal_connect(pipe->videosrc, "pad-added", G_CALLBACK(pad_added_handler), pipe);
+    
+    GstCaps *caps = gst_caps_new_simple("video/x-raw",
+                                        "format", G_TYPE_STRING, "RGBA",
+                                        "width", G_TYPE_INT, 400,
+                                        "height", G_TYPE_INT, 400,
+                                        "pixel-aspect-ratio", GST_TYPE_FRACTION, 1, 1,
+                                        // "framerate", GST_TYPE_FRACTION,0, 1,
+                                        NULL);
+    
+    // Link video elements
+    gboolean link_ok = gst_element_link(pipe->convert, pipe->scale);
+    g_assert(link_ok);
+    link_ok = gst_element_link_filtered(pipe->scale, pipe->appsink, caps);
+    g_assert(link_ok);
+    
+    // Link audio elements
+    link_ok = gst_element_link_many(pipe->audioconvert, pipe->audioresample, pipe->audiosink, NULL);
+    g_assert(link_ok);
+    
+    gst_caps_unref(caps);
+    gst_element_set_state(pipe->pipeline, GST_STATE_READY);
+    g_print("Ready State\n");
+    
+    pipe->bus = gst_element_get_bus(pipe->pipeline);
+    pipe->pipeState = GST_STATE_PLAYING;
+    gst_element_set_state(pipe->pipeline, GST_STATE_PLAYING);
+    g_print("Play State\n");
+    
+    return true;
+}
+
+bool update_pipeline_state(Pipeline *pipe)
+{
+  //Get current state
+  GstState state;
+  gst_element_get_state(pipe->pipeline, &state, NULL, 0);
+  if (state != pipe->pipeState)
+  {
+    g_object_set(pipe->videosrc, "uri", pipe->stream_uri, NULL);
+    gst_element_set_state(pipe->pipeline, pipe->pipeState);
+  }
+
+  return true;
+}
+
+bool check_pipeline_for_message(Pipeline *pipe)
+{
+  pipe->msg = gst_bus_pop_filtered(pipe->bus,
+                                   ( GstMessageType )(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
+  if (pipe->msg != NULL)
+  {
+    GError *err;
+    gchar *debug_info;
+
+    switch (GST_MESSAGE_TYPE(pipe->msg))
+    {
+
+    case GST_MESSAGE_ERROR:
+      gst_message_parse_error(pipe->msg, &err, &debug_info);
+      g_printerr("Error received from element %s: %s\n", GST_OBJECT_NAME(pipe->msg->src), err->message);
+      g_printerr("Debugging information: %s\n", debug_info ? debug_info : "none");
+      g_clear_error(&err);
+      g_free(debug_info);
+      break;
+    case GST_MESSAGE_EOS:
+      g_print("End-Of-Stream reached.\n");
+      if (pipe->loop)
+      {
+        if (!gst_element_seek(pipe->videosrc,
+                              1.0, GST_FORMAT_TIME, GST_SEEK_FLAG_FLUSH,
+                              GST_SEEK_TYPE_SET, 2000000000, //2 seconds (in nanoseconds)
+                              GST_SEEK_TYPE_NONE, GST_CLOCK_TIME_NONE))
+        {
+          g_print("Seek failed!\n");
+        }
+      }
+      break;
+    default:
+      g_printerr("Unexpected message received.\n");
+      break;
+    }
+  }
+  // gst_message_unref(pipe->msg);
+  return true;
+}
+
+bool cleanup_pipeline(Pipeline *pipe)
+{
+  // gst_message_unref (pipe->msg);
+  gst_object_unref(pipe->bus);
+  gst_element_set_state(pipe->pipeline, GST_STATE_NULL);
+  gst_object_unref(pipe->pipeline);
+  // gst_object_unref (pipe->videosrc);
+  // gst_object_unref (pipe->appsink);
+  return true;
+}
 int main(int argc, char* argv[])
 {
     init();
+
+
+    gst_init(&argc, &argv);
+
+    Pipeline ImPipe;
+    create_pipeline(&ImPipe);
 
     glfwSetDropCallback(window, drop_callback);
 
@@ -962,6 +1190,13 @@ int main(int argc, char* argv[])
     colors[ImGuiCol_TabSelected]   = (ImVec4){1, 0.5f, 0.7f, 1.0f};
 
     SingleClickMenu m = {0,.volume = -1};
+
+    GLuint videotex;
+
+    glGenTextures(1, &videotex);
+    glBindTexture(GL_TEXTURE_2D, videotex);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     while (!glfwWindowShouldClose(window))
     {
 
@@ -1001,13 +1236,36 @@ int main(int argc, char* argv[])
 
                     if (igBeginTabItem("Advance", NULL, 0))
                     {
-
                         igEndTabItem();
                     }
 
                     if (igBeginTabItem("Remix", NULL, 0))
                     {
-                       
+                       // GstSample *videosample = gst_app_sink_pull_sample(GST_APP_SINK(ImPipe.appsink));
+                        GstSample *videosample =
+                        gst_app_sink_try_pull_sample(GST_APP_SINK(ImPipe.appsink),GST_SECOND);
+
+                        if (videosample)
+                        {
+                            GstBuffer *videobuf = gst_sample_get_buffer(videosample);
+                            GstMapInfo map;
+
+                            gst_buffer_map(videobuf, &map, GST_MAP_READ);
+
+                            glBindTexture(GL_TEXTURE_2D, videotex);
+                            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA,400,400, 0, GL_RGBA,
+                             GL_UNSIGNED_BYTE, map.data);
+
+                            gst_buffer_unmap(videobuf, &map);
+                            gst_sample_unref(videosample);
+
+                            ImTextureRef tex_ref = {._TexData = NULL, ._TexID = (ImTextureID)videotex};
+
+                            ImDrawList* draw_list = igGetWindowDrawList();
+                            igImage(tex_ref, (ImVec2){400,400}, (ImVec2){0,0}, (ImVec2){1,1}); // Correct UV coordinates
+                        }
+                            update_pipeline_state(&ImPipe);
+                            check_pipeline_for_message(&ImPipe);
                         igEndTabItem();
                     }
                     igEndTabBar();
